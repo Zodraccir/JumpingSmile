185ab7c5f2be7fcddee99c56ba6f37ba741c852f
zodraccir <zichittella.riccardo@gmail.com>
2022-11-26 19:24:31 +0000

    GameEngine Class refactoring

---
+++ include/GameEngine.hpp
@@ -0,0 +1,44 @@
+#ifndef GAME_ENGINE
+#define GAME_ENGINE
+
+#include "common.hpp"
+#include "Player.hpp"
+#include "Texture.hpp"
+#include "Counter.hpp"
+// wrapper class
+class GameEngine
+{
+	public:
+		//Initializes variables    
+		GameEngine();
+
+		//Deallocates memory
+		~GameEngine();
+		
+		//Starts up SDL and creates window
+        bool init();
+
+        //Loads media
+        bool loadMedia();
+
+        bool loop();
+
+        //Frees media and shuts down SDL
+        void close();
+	private:
+        //The window we'll be rendering to
+        SDL_Window* gWindow = NULL;
+
+        //The window renderer
+        SDL_Renderer* gRenderer = NULL;
+
+        //Font
+        TTF_Font *gFont = NULL;
+
+        Player player;
+        Texture texture;
+        Counter counter;
+
+		
+};
+#endif
